aa8f1856b2d02c6e3dcf2a3ea47946fb7a51678c
Julián <julianmmame@gmail.com>
2020-12-11 21:43:05 -0300

    carga de framework -experiencia alterego-

---
+++ js/vars.js
@@ -0,0 +1,4 @@
+var menuHamburguesa = document.getElementById('menuhamburguesa');
+var asidebackground = document.getElementById('asidebackground');
+var aside = document.getElementById('aside');
+var asideCloseButton = document.querySelector('#aside a');
